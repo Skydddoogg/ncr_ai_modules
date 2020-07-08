34e2234d7241a27548209c3cccbf67accc609887
Kykung <59070071@it.kmitl.ac.th>
2020-07-08 21:07:48 +0700

    Add QA bot

---
+++ modules/QABot.py
@@ -0,0 +1,45 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import os
+import global_utils
+import pickle
+
+class SimpleQABot(object):
+    def __init__(self, bot_name):
+        super(SimpleQABot, self).__init__()
+
+        self.bot_name = bot_name
+        self.chatbot_obj = ChatBot(
+            self.bot_name,
+            storage_adapter = 'chatterbot.storage.SQLStorageAdapter',
+            database = self.bot_name + '.sqlite3'
+        )
+        self.trainer = None
+
+    def train(self):
+
+        self.trainer = ChatterBotCorpusTrainer(self.chatbot_obj)
+
+        self.trainer.train(
+            os.path.join(global_utils.ROOT_DIR, 'ResourceBot', 'corpus')
+        )
+
+        return self
+
+    def get_response_text(self, incoming_text):
+
+        return self.chatbot_obj.get_response(incoming_text)
+
+    def play(self):
+
+        if self.trainer == None:
+            self.train()
+
+        while True:
+            text = input("Input: ")
+            if text == 'exit':
+                break
+
+            response = self.get_response_text(text)
+
+            print(response)
--- modules/global_utils.py
+++ modules/global_utils.py
@@ -1,2 +1,6 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def show_module_log(message):
     print('%5s %s' % (' ', message))
